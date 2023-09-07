3e85077b95e57890bf067a928ad1d52cdc1d6535
JeonHR <140233882+JeonHR@users.noreply.github.com>
2023-09-07 12:06:54 +0900

    Add files via upload
    #주석 Revision

--- 2DID_sorting.py
+++ 2DID_sorting.py
@@ -3,11 +3,12 @@ import pandas as pd
 
 
 
-folder_path = './output'  # 삭제하려는 폴더의 경로
+folder_path = './output'  # Python 실행 전 삭제하는 폴더 경로
+
 
 # 폴더 안에 있는 모든 파일을 순회하며 삭제
 for filename in os.listdir(folder_path):
-    file_path = os.path.join(folder_path, filename)
+    file_path = os.path.join(folder_path, filename) ## File Path
     try:
         if os.path.isfile(file_path):
             os.unlink(file_path)
@@ -29,25 +30,28 @@ print(data)
 
 # 출력 폴더가 없다면 생성
 if not os.path.exists(output_folder):
-    os.makedirs(output_folder)
+    os.makedirs(output_folder) ## 폴더 자동생성
 
 # 입력 폴더 내의 모든 CSV 파일에 대해 처리
 for filename in os.listdir(input_folder):
-    if filename.lower().endswith('.csv'):
+    if filename.lower().endswith('.csv'): ###모든 확장자를 소문자로 만드는 방법
         print(f"현재 처리 중인 파일: {filename}")
         input_csv_path = os.path.join(input_folder, filename)
         output_csv_path = os.path.join(output_folder, filename)
         
         # CSV 파일을 DataFrame으로 로드
         df = pd.read_csv(input_csv_path,skiprows=1,index_col=0) ### skiprows csv 시작하는 행을 삭제
+        ## 첫열에 생성되는 값 삭제해주는 과정을 진행
         
         print(df)
         
         try:
             # data 리스트에 포함된 값과 일치하는 행만 선택하여 새로운 DataFrame 생성
+            # 이게 일치하는 행만 확인되게 만드는 핵심
             selected_rows = df[df.index.isin(data)]
             
             # 선택된 행이 비어있지 않은 경우에만 CSV 파일로 저장
+            # 일치하지 않는 것만 형성되는 것 좋은 생각임.
             if not selected_rows.empty:
                 selected_rows.to_csv(output_csv_path)
                 print(f"2 행의 데이터와 일치하는 셀의 열만 남긴 새로운 CSV 파일이 생성되었습니다: {output_csv_path}")
--- rows_sorting.py
+++ rows_sorting.py
@@ -10,7 +10,7 @@ for filename in os.listdir(folder_path):
     file_path = os.path.join(folder_path, filename)
     try:
         if os.path.isfile(file_path):
-            os.unlink(file_path)
+            os.unlink(file_path) ## file 제거하는 함수
             print(f"Deleted: {file_path}")
     except Exception as e:
         print(f"Error deleting {file_path}: {e}")
@@ -36,7 +36,7 @@ if not os.path.exists(output_folder):
 
 # 입력 폴더 내의 모든 CSV 파일에 대해 처리
 for filename in os.listdir(input_folder):
-    if filename.lower().endswith('.CSV'):
+    if filename.lower().endswith('.csv'):
         print(f"현재 처리 중인 파일: {filename}")
         input_csv_path = os.path.join(input_folder, filename)
         output_csv_path = os.path.join(output_folder, filename)
@@ -45,10 +45,10 @@ for filename in os.listdir(input_folder):
         df = pd.read_csv(input_csv_path, skiprows=1) ### skiprows csv 시작하는 행을 삭제
         
         print(df)
-        selected_row = df.loc[:, data]  # 선택할 행의 인덱스를 정확히 지정해주세요.
+        selected_row = df.loc[:, data]  # 선택한 열의 값만 불러오게 만드는 핵심
         
         
         # 새로운 CSV 파일로 저장
-        selected_row.to_csv(output_csv_path, index=False)
+        selected_row.to_csv(output_csv_path, index=False) ## index를 제거하는 것을 통한 깔끔한 값 생성가능
         
         print(f"2DID 의 행 데이터와 일치하는 모든 열만 남긴 새로운 CSV 파일이 생성되었습니다: {output_csv_path}")
